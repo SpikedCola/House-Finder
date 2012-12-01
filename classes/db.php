de17ba1d1acbed79ef46ac7e80aa53ccbf99383c
spikedcola <parkinglotlust@gmail.com>
2012-12-01 00:18:04 -0500

    fixed errors in database class

--- classes/db.php
+++ classes/db.php
@@ -1,17 +1,19 @@
 <?php
 	class Db {
+		public $db;
+		
 		function __construct() {
 			$username = 'root';
 			$password = 'spikedcola';
 			$database = 'mls';
-			$this = mysqli_connect('127.0.0.1', $username, $password, $database);
-			if (mysqli_error($this)) {
+			$db = mysqli_connect('127.0.0.1', $username, $password, $database);
+			if (mysqli_error($this->db)) {
 				die('sql error');
 			}
 		}
 		
 		function user_exists($uniqueId) {
-			$query = $this->prepare('SELECT * FROM unique_ids WHERE unique_id = ?');
+			$query = $this->db->prepare('SELECT * FROM unique_ids WHERE unique_id = ?');
 			
 			$query->bind_param('s', $uniqueId);
 			
@@ -25,7 +27,7 @@
 		}
 		
 		function add_user($uniqueId) {
-			$query = $this->prepare('INSERT INTO unique_ids VALUES unique_id = ?');
+			$query = $this->db->prepare('INSERT INTO unique_ids VALUES unique_id = ?');
 			
 			$query->bind_param('s', $uniqueId);
 			
